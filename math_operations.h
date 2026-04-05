99040d3dce032b941497de1001772ce06e21cc59
valentynvaskiv <valentyn.vaskiv.ki.2022@lpnu.ua>
2026-04-05 22:58:06 +0300

    Task 3

---
+++ math_operations.h
@@ -0,0 +1,12 @@
+#ifndef MATH_OPERATIONS_H
+#define MATH_OPERATIONS_H
+
+/**
+ * @brief Adds two integers together
+ * @param a First integer operand
+ * @param b Second integer operand
+ * @return Sum of the two integers
+ */
+int add(int a, int b);
+
+#endif // MATH_OPERATIONS_H
